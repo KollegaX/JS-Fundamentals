fe27a1ebe2c6943707f63c1042168d4a5c0bcc11
KollegaX <82150731+KollegaX@users.noreply.github.com>
2025-06-14 21:40:54 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -173,3 +173,36 @@ function solve(a,b,c){
 }
 solve(23,6,10)
 
+
+//// From CharCode Table numbers to numbers/signs to signs
+function chars(char1,char2){
+
+    function findBiggerChar(c1,c2){
+        let char1 = c1.charCodeAt(0);
+        let char2 = c2.charCodeAt(0);
+
+        if (char1 < char2){
+            return [char1, char2];
+        } else {
+            return [char2, char1];
+        }    
+    }
+
+    let sortedChars = findBiggerChar(char1, char2);
+    let start = sortedChars[0];
+    let stop = sortedChars[1];
+    // let [start, stop] = findBiggerChar(char1, char2);
+    let result = [];
+    for (let i = start + 1; i < stop; i++){
+        result.push(String.fromCharCode(i));
+
+    }
+    
+
+    console.log(result.join(' '));
+    
+
+}
+chars('#',':')
+
+
