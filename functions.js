3f3c959b0f6e79df763f108d219dcc1b86a59d77
KollegaX <82150731+KollegaX@users.noreply.github.com>
2025-06-15 08:59:57 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -337,3 +337,13 @@ function solve(pwd){
         
     }
 }
+
+
+
+//// Matrix
+function matrix(num){
+    for (let i = 0; i < num; i++){
+        console.log(`${num} `.repeat(num));   
+    }
+}
+matrix(7)
