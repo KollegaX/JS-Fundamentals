9c99e3b0f5f4da5d4f59c899648873ff70445c1c
KollegaX <82150731+KollegaX@users.noreply.github.com>
2025-06-15 08:09:42 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -206,3 +206,58 @@ function chars(char1,char2){
 chars('#',':')
 
 
+
+//// Odd and Even Sum
+function solve(n){
+    function extractEven(num){
+        let result = [];
+        do {
+            let lastNumber = num % 10;
+            if (lastNumber % 2 === 0){
+                result.push(lastNumber);
+            }
+            num = parseInt(num / 10);
+        }
+            while (num > 0){
+                return result;
+        }
+    }
+
+
+    function extractOdd(num){
+        let result = [];
+
+        do {
+            let lastNumber = num % 10;
+            if (lastNumber % 2 > 0){
+                result.push(lastNumber)
+            }
+         num = parseInt(num / 10)   
+
+        } while (num > 0){
+            return result;   
+        }
+
+    }
+
+
+    function sumArray(arr){
+        let result = 0;
+        for (let i = 0; i < arr.length; i++){
+            result += arr[i];
+        }
+        return result;
+    }
+
+
+    let odd = extractOdd(n);
+    let even = extractEven(n);
+    let oddSum = sumArray(odd);
+    let evenSum = sumArray(even);
+    console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`);
+    
+    
+    
+}
+
+solve(123456789)
