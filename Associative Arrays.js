0bcb60f01d1028faed8cffad3e97785f93153132
KollegaX <82150731+KollegaX@users.noreply.github.com>
2025-07-27 15:40:40 +0200

    Update Associative Arrays.js

--- Associative Arrays.js
+++ Associative Arrays.js
@@ -85,6 +85,38 @@ addressBook(['Tim:Doe Crossing',
 'Bill:Ornery Rd'])
 
 
+
+//// 4 [Storage]
+function solve(input){
+    let map = new Map();
+
+
+    for (let data of input){
+        let [item, quantity] = data.split(' ');
+
+        quantity = Number(quantity);
+        
+
+        if (map.has(item)){
+            let existingQuantity = map.get(item);
+
+            let updatedQuantity = existingQuantity + quantity;
+
+            map.set(item, updatedQuantity);
+        } else {
+            map.set(item,quantity);
+        }
+    }
+
+    for (let [key,value] of map.entries()){
+        console.log(`${key} -> ${value}`);
+        
+    }
+}
+solve(['tomatoes 10','coffee 5','olives 100','coffee 40'])
+
+
+
 //// 5 [School grades]
 function solve(input){
     // let map = new Map();
